1c8d8f2a0d29aac1400dc133991a0b3b3606807e
use1482 <102653515+use1482@users.noreply.github.com>
2022-04-22 14:28:44 +0200

    Add files via upload

---
+++ Calculadora2/calculadora.js
@@ -0,0 +1,18 @@
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+    exp(num1, num2) {
+        return Math.pow(num1, num2);
+    }
+}
+
